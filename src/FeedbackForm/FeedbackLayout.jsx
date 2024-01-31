430530b2484e6325805c2aa126b8f767f4838e1c
Samuvel L <samdeveloper7550@gmail.com>
2024-01-31 08:03:02 +0530

    alignment

--- src/FeedbackForm/FeedbackLayout.jsx
+++ src/FeedbackForm/FeedbackLayout.jsx
@@ -9,7 +9,7 @@ const FeedbackLayout = () => {
   
   return (
     <>
-        <div className='flex flex-col gap-6 lg:flex-row md:flex-row px-3.5 py-5 lg:py-8 lg:px-16 md:py-4 md:px-8 items-center'>
+        <div className='flex h-screen w-screen flex-col gap-6 lg:flex-row md:flex-row px-3.5 py-5 lg:py-8 lg:px-16 md:py-4 md:px-8 items-center'>
             <section>
                 <div className="flex flex-col gap-3 h-full md:gap-8 lg:gap-8  ">
                 <div>
@@ -22,7 +22,7 @@ const FeedbackLayout = () => {
                 </div>
                 </div>
             </section>
-            <section>
+            <section className='flex h-full w-full items-center justify-center align-middle'>
              <Outlet/>
             </section>
         </div>
--- src/Onsuccess.jsx
+++ src/Onsuccess.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Onsuccess = () => {
   return (
-    <div className='flex flex-col px-3.5 py-3.5 items-center justify-center h-screen  drop-shadow-md text-white'>
+    <div className='flex flex-col px-3.5 py-3.5 items-center justify-center h-full  drop-shadow-md text-white'>
       <h1 className='text-4xl pl-6 font-bold place-content-center'>Successful! Thank you for your valuable feedback.</h1>
       <h6 className='mt-4 text-xl'>Your feedback has been submitted successfully.</h6>
     </div>
